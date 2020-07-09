9c68cd7f66c53cc49ce552369e957dfe2d1c73b8
Hung Vu <hungminhvu94@gmail.com>
2020-07-09 11:00:45 +0700

    add google font and page pre-loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// pre-loader
+$("body").prepend('<div class="mask"></div>');
 $(document).ready(function () {
-    
+    $(".mask").css("display", "none");
+    $("#intro-loader").css("display", "none");
 });

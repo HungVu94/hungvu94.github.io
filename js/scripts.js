89437384504d2a297ba0d81f29a1778c7593c39b
Hung Vu <hungminhvu94@gmail.com>
2020-07-10 12:39:05 +0700

    finish the about section

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     // hide pre-loader after page finish loading
     $(".mask").css("display", "none");
     $("#intro-loader").css("display", "none");
+    //init wow js
+    new WOW().init();
     //nav bar on load
     if ($(window).scrollTop() > 0) {
         $(".navbar").addClass("darken");
@@ -63,4 +65,81 @@ $(document).ready(function () {
         $("li.home-bg-slide:last-child").removeClass("active-slide");
         $("li.home-bg-slide:first-child").addClass("active-slide");
     };
+    // scrolling to section for menu
+    $("#my-nav a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        // Store hash
+        var hash = this.hash;
+        $('#my-nav li').removeClass('active');
+        $('#my-nav li').has("a[href='"+hash+"']").addClass('active');
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top //scroll top position of the selected section
+        }, 800, function(){
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        }
+    });
+    // highlight section when scroll to it
+        // highlight section when page finish loading
+    $('section').each(function() {
+        var target = $(this).offset().top;
+        var id = $(this).attr('id');
+        if ($(window).scrollTop() >= target) {
+            $('#my-nav li').removeClass('active');
+            $('#my-nav li').has("a[href='#"+id+"']").addClass('active');
+        }
+    });
+    $(window).scroll(function() {
+        var position = $(this).scrollTop() + 75;
+        $('section').each(function() {
+            var target = $(this).offset().top;
+            var id = $(this).attr('id');
+            if (position >= target) {
+                $('#my-nav li').removeClass('active');
+                $('#my-nav li').has("a[href='#"+id+"']").addClass('active'); //find the id of each target section then when scroll to them, the link that has href = #id will be highlighted
+            }
+        });
+    });
+    // counter
+    $(window).scroll(testScroll);
+    var viewed_counter = false;
+    // Determine when element is visible in browser window
+    function scrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop(); //top of browser window
+        var docViewBottom = docViewTop + $(window).height(); //bottom of browser window
+        var elemTop = $(elem).offset().top; //top of element
+        var elemBottom = elemTop + $(elem).height(); //bottom of element
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)); //return the value when element is inside browser window
+    }
+    // Determine when element is outside browser window
+    function scrolledVisibleView(elem) {
+        var docViewTop = $(window).scrollTop(); //top of browser window
+        var docViewBottom = docViewTop + $(window).height(); //bottom of browser window
+        var elemTop = $(elem).offset().top; //top of element
+        var elemBottom = elemTop + $(elem).height(); //bottom of element
+        return ((elemBottom <= docViewBottom) || (elemTop >= docViewTop)); //return the value when element is outside browser window
+    }
+    //counter
+    function testScroll() {
+        if (scrolledIntoView($(".number-counters")) && !viewed_counter) { //if the counter start enter browser window and not view yet then start run counter
+            viewed_counter = true; //being viewed
+            $('.count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                    }, {
+                        duration: 1500,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now)); //round up the data number
+                        }
+                });
+            });
+        }
+    }
 });
